ea865be6711d212627541adcdf4134573eb37c32
skwh <somekidwithhtml@gmail.com>
2012-12-03 19:16:14 -0700

    Worked on Weather API

--- Lesson_0/EXTREME Advanced/Weather API/script.js
+++ Lesson_0/EXTREME Advanced/Weather API/script.js
@@ -1,1 +1,17 @@
-// JavaScript Document
+function load() {
+	var request = $.ajax({
+		type: "GET",
+		url: "http://weather.yahooapis.com/forecastrss?w=12793016&u=f"
+	}).done(function(rss) {
+		rssProcess(rss);
+	});
+}
+
+function rssProcess(rssText) {
+	//placeholder operation
+	$('body').append(rssText);
+}
+
+$(document).ready(function() {
+	load();
+});
