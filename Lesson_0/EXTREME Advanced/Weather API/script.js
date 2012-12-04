adea61d67d0872fdecff8e4fe38410cafdba5e14
skwh <somekidwithhtml@gmail.com>
2012-12-04 07:20:13 -0700

    Added xml

--- Lesson_0/EXTREME Advanced/Weather API/script.js
+++ Lesson_0/EXTREME Advanced/Weather API/script.js
@@ -1,10 +1,6 @@
 function load() {
-	var request = $.ajax({
-		type: "GET",
-		url: "http://weather.yahooapis.com/forecastrss?w=12793016&u=f"
-	}).done(function(rss) {
-		rssProcess(rss);
-	});
+	var text = $.get("../Weather API/document.xml");
+	rssProcess(text);
 }
 
 function rssProcess(rssText) {
